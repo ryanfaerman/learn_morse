8bc58b74607b65523e7e0d5df79af415140e6ff9
Ryan Faerman <ry@nwitty.com>
2013-03-20 23:22:10 -0400

    testing latest letter playing

--- src/co/wtty/apps/learnmorse/MainActivity.java
+++ src/co/wtty/apps/learnmorse/MainActivity.java
@@ -3,6 +3,8 @@ package co.wtty.apps.learnmorse;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,16 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button _button = (Button) findViewById(R.id.play_tone_button);
+		_button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				LetterPlayer _player = LetterPlayer.getInstance(getApplicationContext());
+				_player.play("S");
+			}
+		});
 	}
 
 	@Override
@@ -18,5 +30,7 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
+	
+	
 
 }
